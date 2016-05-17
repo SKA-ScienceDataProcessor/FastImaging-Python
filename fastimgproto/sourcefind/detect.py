Fix off-by-one error in source detection.
ndimage labels range over 1-N. 0 is background!

fastimgproto/sourcefind/detect.py
@@ -2,15 +2,15 @@ import astropy.stats
 import numpy as np
 from scipy import ndimage
 
-def extract_sources(image, detection_n_sigma, analysis_n_sigma):
-    rms = estimate_rms(image)
+def extract_sources(image, detection_n_sigma, analysis_n_sigma,
+                    rms_estimate):
     bg_level = np.ma.median(image)
-    detection_thresh = bg_level + detection_n_sigma*rms
-    analysis_thresh = bg_level + analysis_n_sigma*rms
+    detection_thresh = bg_level + detection_n_sigma*rms_estimate
+    analysis_thresh = bg_level + analysis_n_sigma*rms_estimate
     analysis_map = image > analysis_thresh
     labels_map, n_labels = ndimage.label(analysis_map)
     detections = []
-    for i in range(n_labels):
+    for i in range(1, n_labels+1):
         extraction =  process_island(image,labels_map, i, detection_thresh)
         if extraction:
             detections.append(extraction)

fastimgproto/tests/test_fixtures/test_image_fixtures.py
@@ -12,4 +12,5 @@ def test_model_generation_and_evaluation():
     src = gaussian_point_source(x_centre=32, y_centre=64)
     img += evaluate_model_on_pixel_grid(img.shape, src)
     assert img[0, 0] == 0.0
-    assert np.abs(img[src.y_mean, src.x_mean] - src.amplitude) < 0.01
+    assert np.abs(img[int(src.y_mean.value), int(src.x_mean.value)]
+                    - src.amplitude) < 0.01

fastimgproto/tests/test_sourcefind/test_detection.py
@@ -6,22 +6,50 @@ from fastimgproto.tests.fixtures.image import (
 import fastimgproto.sourcefind.detect as detect
 import numpy as np
 
+ydim = 128
+xdim = 64
+rms = 1.0
+bright_src = gaussian_point_source(x_centre=48, y_centre=52, amplitude=10.0)
+faint_src = gaussian_point_source(x_centre=32, y_centre=64, amplitude=3.5)
 
-def test_basic_source_detection():
-    ydim = 128
-    xdim = 64
-    img = uncorrelated_gaussian_noise_background((ydim, xdim))
-    bright_src = gaussian_point_source(x_centre=32, y_centre=64, amplitude=5.0)
-    faint_src = gaussian_point_source(x_centre=64, y_centre=64, amplitude=3.5)
+
+def test_rms_estimation():
+    img = uncorrelated_gaussian_noise_background(shape=(ydim, xdim),
+                                                 sigma=rms)
     img += evaluate_model_on_pixel_grid(img.shape, bright_src)
     img += evaluate_model_on_pixel_grid(img.shape, faint_src)
+    rms_est= detect.estimate_rms(img)
+    # print "RMS EST:", rms_est
+    assert np.abs((rms_est - rms) / rms) < 0.05
 
-    srcs = detect.extract_sources(img, detection_n_sigma=4.5,
-                                  analysis_n_sigma=3)
-    # We expect to detect the bright source, but not the faint one.
+def test_basic_source_detection():
+    """
+    We use a flat background (rather than noisy) to avoid random-noise fluctuations
+    causing erroneous detections (and test-failures).
+    """
+    img = np.zeros((ydim, xdim))
+    img += evaluate_model_on_pixel_grid(img.shape, bright_src)
+    # img += evaluate_model_on_pixel_grid(img.shape, faint_src)
+
+    srcs = detect.extract_sources(img, detection_n_sigma=4,
+                                  analysis_n_sigma=3,
+                                  rms_estimate=rms)
     assert len(srcs) == 1
     pixel_idx, pixel_value = srcs[0]
     y_idx, x_idx = pixel_idx
-    assert np.abs(y_idx - bright_src.y_mean) <0.5
-    assert np.abs(x_idx - bright_src.x_mean) < 0.5
+    assert np.abs(y_idx - bright_src.y_mean) <1.1
+    assert np.abs(x_idx - bright_src.x_mean) < 1.1
+
+    # We expect to detect the bright source, but not the faint one.
+    img += evaluate_model_on_pixel_grid(img.shape, faint_src)
+    srcs = detect.extract_sources(img, detection_n_sigma=4,
+                                  analysis_n_sigma=3,
+                                  rms_estimate=rms)
+    assert len(srcs) == 1
+    # Unless we add it again and effectively double the faint_src flux
+    img += evaluate_model_on_pixel_grid(img.shape, faint_src)
+    srcs = detect.extract_sources(img, detection_n_sigma=4,
+                                  analysis_n_sigma=3,
+                                  rms_estimate=rms)
+    assert len(srcs) == 2
 