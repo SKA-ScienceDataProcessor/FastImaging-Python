Minor bugfix - integer input gave incorrect results.
The output array was allocated as 'zeros_like' the integer input.
This resulted in float output values being cast to integers
(typically zero) before the output array was returned!

src/fastimgproto/gridder/conv_funcs.py
@@ -8,6 +8,7 @@ callable with one parameter, the distance in pixels.
 
 This allows us to pass the convolution routine the minimum of extra parameters.
 """
+from __future__ import division
 import numpy as np
 from six import add_metaclass
 from abc import ABCMeta, abstractmethod
@@ -37,7 +38,7 @@ class ConvFuncBase(object):
 
     def __call__(self, radius_in_pix):
         radius_in_pix = np.atleast_1d(radius_in_pix)
-        output = np.zeros_like(radius_in_pix)
+        output = np.zeros_like(radius_in_pix, dtype=np.float)
         inside_trunc_radius = np.fabs(radius_in_pix) < self.trunc
         output[inside_trunc_radius] = self.f(radius_in_pix[inside_trunc_radius])
         return output

tests/test_gridder/test_conv_funcs.py
@@ -33,6 +33,9 @@ def test_triangle_func():
 
     check_function_results_close(triangle2, io_pairs)
 
+    assert triangle2(1) == triangle2(1.0)
+    assert triangle2(1).dtype == np.float
+
 
 def test_tophat_func():
     tophat3 = conv_funcs.Pillbox(half_base_width=3.0)
@@ -75,7 +78,7 @@ def test_gaussian_sinc():
     w1 = 2.52
     w2 = 1.55
     trunc = 5.0
-    sinc = conv_funcs.GaussianSinc(trunc=trunc, w1=w1, w2=w2)
+    g_sinc = conv_funcs.GaussianSinc(trunc=trunc, w1=w1, w2=w2)
     io_pairs = np.array([
         [0.0, 1.0],
         [w2 * 0.5,
@@ -88,4 +91,7 @@ def test_gaussian_sinc():
          np.exp(-1. * (2.5 * w2 / w1) ** 2) * 1. / (2.5 * np.pi)],
         [5.5, 0.],  # Truncated
     ])
-    check_function_results_close(sinc, io_pairs)
+    check_function_results_close(g_sinc, io_pairs)
+
+    assert g_sinc(1) == g_sinc(1.0)
+    assert g_sinc(1).dtype == np.float