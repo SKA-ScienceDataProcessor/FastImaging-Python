30334cd42056b5c4bf229798828ff010ec2a8e07
Tim Staley <git@timstaley.co.uk>
2017-01-25 15:17:41 +0000

    Add handy profiled wrapper function

---
+++ src/fastimgproto/fixtures/profiling.py
@@ -0,0 +1,6 @@
+from fastimgproto.sourcefind.image import SourceFindImage
+from memory_profiler import profile
+
+@profile
+def memprof_sourcefindimage(data, detection_n_sigma, analysis_n_sigma):
+    return SourceFindImage(data = data, detection_n_sigma=5, analysis_n_sigma=3)
