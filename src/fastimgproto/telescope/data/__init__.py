b144b1f59966ce3ec80e3cc9a05a7b0e82e9e736
Tim Staley <git@timstaley.co.uk>
2017-02-07 16:28:12 +0000

    Add VLA ITRF files

--- src/fastimgproto/telescope/data/__init__.py
+++ src/fastimgproto/telescope/data/__init__.py
@@ -5,3 +5,12 @@ telescope_data_dir = __path__[0]
 meerkat_itrf_filepath = os.path.join(telescope_data_dir,
                                        'meerkat.itrf.txt')
 
+vla_a_filepath = os.path.join(telescope_data_dir,
+                                       'vlaa.itrf.txt')
+vla_b_filepath = os.path.join(telescope_data_dir,
+                                       'vlab.itrf.txt')
+vla_c_filepath = os.path.join(telescope_data_dir,
+                                       'vlac.itrf.txt')
+vla_d_filepath = os.path.join(telescope_data_dir,
+                                       'vlad.itrf.txt')
+
--- src/fastimgproto/telescope/readymade.py
+++ src/fastimgproto/telescope/readymade.py
@@ -1,7 +1,27 @@
 from .base import (Telescope, parse_itrf_ascii_to_xyz_and_labels)
-from .data import (meerkat_itrf_filepath)
+from .data import (meerkat_itrf_filepath,
+                   vla_a_filepath, vla_b_filepath,
+                   vla_c_filepath, vla_d_filepath
+                   )
 
 
 def Meerkat():
     return Telescope.from_itrf(
         *parse_itrf_ascii_to_xyz_and_labels(meerkat_itrf_filepath))
+
+
+def VLA_A():
+    return Telescope.from_itrf(
+        *parse_itrf_ascii_to_xyz_and_labels(vla_a_filepath))
+
+def VLA_B():
+    return Telescope.from_itrf(
+        *parse_itrf_ascii_to_xyz_and_labels(vla_b_filepath))
+
+def VLA_C():
+    return Telescope.from_itrf(
+        *parse_itrf_ascii_to_xyz_and_labels(vla_c_filepath))
+
+def VLA_D():
+    return Telescope.from_itrf(
+        *parse_itrf_ascii_to_xyz_and_labels(vla_d_filepath))
