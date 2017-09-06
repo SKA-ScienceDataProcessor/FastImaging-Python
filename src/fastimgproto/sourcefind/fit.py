Minor fixes to gaussian2d jacobian.
Transpose the output, so we're effectively giving a list of
jacobians, one for an evaluation at each x,y pair. This matches
the behaviour of the plain gaussian2d function, and seems to
give the shape of array expected by least_squares.

Unfortunately, employing the jacobian (in the current tests)
doesn't seem to work well - most fits are much worse to the point
of being failures, even though it supposedly converged.

src/fastimgproto/sourcefind/fit.py
@@ -55,7 +55,6 @@ def _valid_theta(instance, attribute, value):
         raise ValueError("Theta should lie in the range (-pi/2,pi/2].")
 
 
-
 @attrs(frozen=True)
 class Gaussian2dParams(object):
     """
@@ -146,11 +145,11 @@ class Gaussian2dParams(object):
         # fits can easily stray into negative values:
         semimajor = np.fabs(semimajor)
         semiminor = np.fabs(semiminor)
-        half_pi = np.pi/2.
+        half_pi = np.pi / 2.
         if semimajor < semiminor:
             semimajor, semiminor = semiminor, semimajor
             theta = theta + half_pi
-        mod_theta = math.fmod(theta, np.pi) # Rotations by pi are degeneracies
+        mod_theta = math.fmod(theta, np.pi)  # Rotations by pi are degeneracies
         # This gets us to the range (-pi,pi). Now we add/subtract an additional
         # pi as required to get down to (-pi/2, pi/2).
         if mod_theta <= -half_pi:
@@ -225,7 +224,8 @@ def gaussian2d(x, y, x_centre, y_centre, amplitude, x_stddev, y_stddev, theta):
                                 (c * ydiff ** 2)))
 
 
-def gaussian2d_jac(x, y, x_centre, y_centre, amplitude, x_stddev, y_stddev, theta):
+def gaussian2d_jac(x, y, x_centre, y_centre, amplitude, x_stddev, y_stddev,
+                   theta):
     """
     Jacobian of Gaussian2d.
 
@@ -276,7 +276,9 @@ def gaussian2d_jac(x, y, x_centre, y_centre, amplitude, x_stddev, y_stddev, thet
                        db_dtheta * xdiff * ydiff +
                        dc_dtheta * ydiff2))
 
-    return [dg_dx_mean, dg_dy_mean,
-            dg_dA,
-            dg_dx_stddev, dg_dy_stddev,
-            dg_dtheta]
+    return np.array([dg_dx_mean,
+                     dg_dy_mean,
+                     dg_dA,
+                     dg_dx_stddev,
+                     dg_dy_stddev,
+                     dg_dtheta]).T

src/fastimgproto/sourcefind/image.py
@@ -8,7 +8,11 @@ from attr import attrib, attrs
 from scipy import ndimage
 from scipy.optimize import least_squares
 
-from fastimgproto.sourcefind.fit import Gaussian2dParams, gaussian2d
+from fastimgproto.sourcefind.fit import (
+    Gaussian2dParams,
+    gaussian2d,
+    gaussian2d_jac,
+)
 from fastimgproto.utils import (
     nonzero_bounding_slice_2d,
     positive_negative_sign_validator,
@@ -300,8 +304,8 @@ class SourceFindImage(object):
 
     def fit_gaussian_2d(self, island, verbose=0):
         # x, y, x_centre, y_centre, amplitude, x_stddev, y_stddev, theta
-        ygrid, xgrid = island.unmasked_pixel_indices
-        fitting_data = island.data[ygrid, xgrid]
+        y_indices, x_indices = island.unmasked_pixel_indices
+        fitting_data = island.data[y_indices, x_indices]
 
         def island_residuals(x_centre,
                              y_centre,
@@ -310,7 +314,7 @@ class SourceFindImage(object):
                              semiminor,
                              theta):
             """
-            A wrapped version of gaussian_2d applied to this island's unmasked
+            A wrapped version of `gaussian2d` applied to this island's unmasked
             pixels, then subtracting the island values
 
             Same args as :ref:`.gaussian2d`, except without the `xgrid,ygrid`
@@ -321,7 +325,7 @@ class SourceFindImage(object):
 
             """
 
-            model_vals = gaussian2d(xgrid, ygrid,
+            model_vals = gaussian2d(x_indices, y_indices,
                                     x_centre=x_centre,
                                     y_centre=y_centre,
                                     amplitude=amplitude,
@@ -331,6 +335,25 @@ class SourceFindImage(object):
                                     )
             assert model_vals.shape == fitting_data.shape
             return fitting_data - model_vals
+
+        def located_jacobian(pars):
+            """
+            Wrapped version of `gaussian2d_jac` applied at these pixel positions.
+            """
+            (x_centre,
+             y_centre,
+             amplitude,
+             semimajor,
+             semiminor,
+             theta) = pars
+            return gaussian2d_jac(x_indices, y_indices,
+                                  x_centre=x_centre,
+                                  y_centre=y_centre,
+                                  amplitude=amplitude,
+                                  x_stddev=semimajor,
+                                  y_stddev=semiminor,
+                                  theta=theta,
+                                  )
 
         def wrapped_island_residuals(pars):
             """
@@ -356,9 +379,12 @@ class SourceFindImage(object):
                                           semiminor=1.,
                                           theta=0
                                           )
+
+        # Using the jacobian mostly gives bad fits?
         lsq_result = least_squares(fun=wrapped_island_residuals,
+                                   # jac=located_jacobian,
                                    x0=attr.astuple(initial_params),
-                                   # method='lm',
+                                   # method='dogbox',
                                    verbose=verbose,
                                    )
         island.params.leastsq_fit = Gaussian2dParams.from_unconstrained_parameters(

tests/test_sourcefind/test_fitting.py
@@ -41,14 +41,38 @@ def test_gauss2d_func_and_jacobian():
 
     pars = attr.astuple(profile)
     n_tests_run = 0
-    for x in rstate.uniform(profile.x_centre - profile.semimajor * 5.,
-                            profile.x_centre + profile.semimajor * 5.,
-                            size=n_samples
-                            ):
-        for y in rstate.uniform(profile.y_centre - profile.semimajor * 5.,
-                                profile.y_centre + profile.semimajor * 5.,
-                                size=n_samples
-                                ):
+
+    x_samples = rstate.uniform(profile.x_centre - profile.semimajor * 5.,
+                               profile.x_centre + profile.semimajor * 5.,
+                               size=n_samples
+                               )
+    y_samples = rstate.uniform(profile.y_centre - profile.semimajor * 5.,
+                               profile.y_centre + profile.semimajor * 5.,
+                               size=n_samples
+                               )
+
+    xy_pairs = np.column_stack((x_samples, y_samples))
+
+    looped_g2d = np.array(
+        [gaussian2d(*tuple(pair), *attr.astuple(profile))
+         for pair in xy_pairs])
+    vectored_g2d = gaussian2d(x_samples, y_samples, *attr.astuple(profile))
+    assert (looped_g2d == vectored_g2d).all()
+
+    looped_jac = np.array(
+        [gaussian2d_jac(*tuple(pair), *attr.astuple(profile))
+         for pair in xy_pairs])
+    vectored_jac = gaussian2d_jac(x_samples, y_samples, *attr.astuple(profile))
+
+    print(looped_jac.shape)
+    print(vectored_jac.shape)
+    assert (looped_jac.shape == vectored_jac.shape)
+    assert (looped_jac == vectored_jac).all()
+
+    # assert
+
+    for x in x_samples:
+        for y in y_samples:
             # Need to partially bind the functions for each x/y location
             def located_g2d(x_centre, y_centre, amplitude, x_stddev, y_stddev,
                             theta):
@@ -76,7 +100,7 @@ def test_gauss2d_func_and_jacobian():
                 n_tests_run, n_samples ** 2))
             logger.debug("Checking gradient at x/y position {} {}".format(x, y))
 
-            eps = np.sqrt(np.finfo(float).eps) # default for check_grad
+            eps = np.sqrt(np.finfo(float).eps)  # default for check_grad
             # eps = 1e-12
 
             numerical = scipy.optimize.approx_fprime(pars, wrapped_gaussian2d,
@@ -96,7 +120,7 @@ def test_gauss2d_func_and_jacobian():
             #     check_grad_result))
             # logger.debug("")
 
-            np.testing.assert_allclose(analytic,numerical,
+            np.testing.assert_allclose(analytic, numerical,
                                        atol=1e-7)
             assert check_grad_result == approx(0., abs=2e-6)
 