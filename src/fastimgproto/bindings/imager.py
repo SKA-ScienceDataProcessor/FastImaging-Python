Some improvements and fixes. Implemented output dirty image masking based on near zero regions of the primary beam.

src/fastimgproto/bindings/imager.py
@@ -98,8 +98,8 @@ def cpp_image_visibilities(vis,
                            obs_ra=0.0,
                            lha=np.ones(1,),
                            pbeam_coefs=np.array([1]),
-                           aproj_interp_rotation=False,
-                           aproj_optimisation=False
+                           aproj_opt=False,
+                           aproj_mask_perc=0.0
 ):
     """
     Convenience wrapper over _cpp_image_visibilities.
@@ -173,10 +173,11 @@ def cpp_image_visibilities(vis,
             1d array, shape: `(n_vis,)`.
         pbeam_coefs (numpy.ndarray): Primary beam given by spherical harmonics coefficients.
             The SH degree is constant being derived from the number of coefficients minus one.
-        aproj_interp_rotation (bool): Use interpolation techniques for primary beam rotation
-            in A-projection instead of recomputing a-kernel from spherical harmonics.
-        aproj_optimisation (bool): Use A-projection optimisation which rotates the
-            convolution kernel rather than the A-kernel.
+        aproj_opt (bool): Use A-projection optimisation which rotates the convolution
+            kernel rather than the A-kernel.
+        aproj_mask_perc (float): Threshold value (in percentage) used to detect near zero
+            regions of the primary beam. The output dirty image and beam are masked in
+            the same regions to hide the boosted noise.
 
     Returns:
         tuple: (image, beam)
@@ -225,8 +226,8 @@ def cpp_image_visibilities(vis,
         obs_ra,
         lha,
         pbeam_coefs,
-        aproj_interp_rotation,
-        aproj_optimisation,
+        aproj_opt,
+        aproj_mask_perc,
     )
 
     return image, beam

src/fastimgproto/gridder/akernel_generation.py
@@ -3,21 +3,21 @@ from scipy.special import sph_harm
 from scipy import ndimage
 
 
-def generate_primary_beam(pbeam_coefs, fov, workarea_size, rot_angle=0.0):
+def generate_akernel(pbeam_coefs, fov, kernel_size, rot_angle=0.0):
     """
-    Generate rotated primary beam for A-projection using spherical harmonics and rotation angle provided.
+    Generate rotated A-kernel for A-projection using spherical harmonics and rotation angle provided.
 
     Args:
         pbeam_coefs (numpy.ndarray): Spherical harmonic coefficients that generate the primary beam.
         fov (float): Field od view, in radians
-        workarea_size (int): Workarea size for kernel generation.
+        kernel_size (int): Workspace size for kernel generation.
         rot_angle (float): Rotation angle (added to theta angle of spherical harmonics).
 
     Returns:
-        numpy.ndarray: Rotated primary beam
+        numpy.ndarray: Rotated A-kernel
     """
 
-    distance_radians = np.linspace(-(fov/2), (fov/2), workarea_size)
+    distance_radians = np.linspace(-(fov/2), (fov/2), kernel_size)
     x_rad, y_rad = np.meshgrid(distance_radians, distance_radians)
 
     # Compute phi and theta
@@ -29,20 +29,20 @@ def generate_primary_beam(pbeam_coefs, fov, workarea_size, rot_angle=0.0):
     sh_degree = len(pbeam_coefs) - 1
 
     # Create primary beam from spherical harmonics
-    pbeam = np.zeros((workarea_size, workarea_size))
+    pbeam = np.zeros((kernel_size, kernel_size))
     for ord in range(0, len(pbeam_coefs)):
         if pbeam_coefs[ord] != 0.0:
             pbeam += pbeam_coefs[ord] * np.abs(sph_harm(ord, sh_degree, theta + rot_angle, phi).real)
 
     # Invert primary beam function (and normalize)
-    pbeam = np.max(np.max(pbeam)) / pbeam
+    akernel = np.max(np.max(pbeam)) / pbeam
 
-    return pbeam
+    return akernel
 
 
-def generate_primary_beam_for_lha(pbeam_coefs, lha, dec_rad, ra_rad, fov, workarea_size):
+def generate_akernel_from_lha(pbeam_coefs, lha, dec_rad, ra_rad, fov, kernel_size):
     """
-    Generate rotated primary beam for A-projection using spherical harmonics and the specified LHA value.
+    Generate rotated A-kernel for A-projection using spherical harmonics and the specified LHA value.
 
     Args:
         pbeam_coefs (numpy.ndarray): Spherical harmonic coefficients that generate the primary beam.
@@ -50,43 +50,44 @@ def generate_primary_beam_for_lha(pbeam_coefs, lha, dec_rad, ra_rad, fov, workar
         dec_rad (float): Declination of the observation pointing centre, in radians
         ra_rad (float): Right Ascension of the observation pointing centre, in radians
         fov (float): Field od view, in radians
-        workarea_size (int): Workarea size for kernel generation.
+        kernel_size (int): Workspace size for kernel generation.
 
     Returns:
-        numpy.ndarray: Rotated primary beam
+        numpy.ndarray: Rotated A-kernel
     """
 
     # Determine parallactic angle (in radians)
     pangle = parallatic_angle(lha, dec_rad, ra_rad)
 
-    pbeam = generate_primary_beam(pbeam_coefs, fov, workarea_size, pangle)
+    akernel = generate_akernel(pbeam_coefs, fov, kernel_size, pangle)
 
-    return pbeam
+    return akernel
 
 
-def rotate_primary_beam_for_lha(pbeam, lha, dec_rad, ra_rad):
+def rotate_akernel_by_lha(akernel, lha, dec_rad, ra_rad):
     """
-    Rotate input primary beam matrix using interpolation and the specified LHA value.
+    Rotate input A-kernel matrix using interpolation and the specified LHA value.
 
     Args:
-        pbeam (numpy.ndarray): Primary beam matrix.
+        akernel (numpy.ndarray): Primary beam matrix.
         lha (float): Local hour angle of the object, in decimal hours (0,24)
         dec_rad (float): Declination of the observation pointing centre, in radians
         ra_rad (float): Right Ascension of the observation pointing centre, in radians
 
     Returns:
-        numpy.ndarray: Rotated primary beam
+        numpy.ndarray: Rotated A-kernel
     """
 
     # Determine parallactic angle (in radians)
     pangle = parallatic_angle(lha, dec_rad, ra_rad)
 
-    pbmin_r = pbeam[0, 0]
+    akmin_r = akernel[0, 0]
 
     # Rotate (use order-1 spline interpolation)
-    a_kernel = ndimage.interpolation.rotate(pbeam, np.rad2deg(pangle), reshape=False, mode='constant', cval=pbmin_r, order=1)
+    rot_akernel = ndimage.interpolation.rotate(akernel, np.rad2deg(pangle), reshape=False, mode='constant',
+                                               cval=akmin_r, order=1)
 
-    return a_kernel
+    return rot_akernel
 
 
 def parallatic_angle(lha, dec_rad, ra_rad):

src/fastimgproto/gridder/gridder.py
@@ -12,11 +12,13 @@ from scipy import ndimage
 
 from fastimgproto.gridder.wkernel_generation import WKernel
 from fastimgproto.gridder.kernel_generation import Kernel, ImgDomKernel
-import fastimgproto.gridder.akernel_generation as akernel
+from fastimgproto.gridder import akernel_generation
 
 from fastimgproto.utils import reset_progress_bar
 
 logger = logging.getLogger(__name__)
+
+aproj_interp_rotation = False
 
 
 def convolve_to_grid(kernel_func,
@@ -42,8 +44,7 @@ def convolve_to_grid(kernel_func,
                      obs_ra=0.0,
                      lha=np.ones(1, ),
                      pbeam_coefs=np.array([1]),
-                     aproj_interp_rotation=False,
-                     aproj_optimisation=False,
+                     aproj_opt=False,
                      raise_bounds=False,
                      progress_bar=None):
     """
@@ -120,21 +121,21 @@ def convolve_to_grid(kernel_func,
             1d array, shape: `(n_vis,)`.
         pbeam_coefs (numpy.ndarray): Primary beam given by spherical harmonics coefficients.
             The SH degree is constant being derived from the number of coefficients minus one.
-        aproj_interp_rotation (bool): Use interpolation techniques for primary beam rotation
-            in A-projection instead of recomputing a-kernel from spherical harmonics.
-        aproj_optimisation (bool): Use A-projection optimisation which rotates the
-            convolution kernel rather than the A-kernel.
+        aproj_opt (bool): Use A-projection optimisation which rotates the convolution
+            kernel rather than the A-kernel.
         raise_bounds (bool): Raise an exception if any of the UV samples lie
             outside (or too close to the edge) of the grid.
         progress_bar (tqdm.tqdm): [Optional] progressbar to update.
 
     Returns:
-        tuple: (vis_grid, sampling_grid)
-            Tuple of ndarrays representing the gridded visibilities and the
-            sampling weights.
-            These are 2d arrays of same dtype as **vis**,
+        tuple: (vis_grid, sampling_grid, lha_planes)
+            Tuple of ndarrays representing the gridded visibilities, the
+            sampling weights, and the list of LHA planes (when A-projection
+            is used).
+            The first two are 2d arrays of same dtype as **vis**,
             shape ``(image_size, image_size)``.
             Note numpy style index-order, i.e. access like ``vis_grid[v,u]``.
+            The last one is 1d array os shape `(aproj_numtimesteps,)`
 
     """
     if oversampling is None:
@@ -242,7 +243,9 @@ def convolve_to_grid(kernel_func,
         num_wplanes = 1
         w_planes_gvidx = [0, len(good_vis_idx)]
 
+
     # Compute A-Projection time intervals
+    lha_planes = []
     if use_aproj is True:
         min_time = np.min(lha)
         max_time = np.max(lha)
@@ -250,22 +253,21 @@ def convolve_to_grid(kernel_func,
         tstep_size = (max_time-min_time)/num_timesteps
         time_intervals = np.linspace(min_time-tstep_size/2, max_time+tstep_size/2, aproj_numtimesteps+1)
         # Compute average lha value for each interval
-        lha_mean = []
         vis_timestep = np.zeros_like(vis, dtype=int)
         for ts in range(num_timesteps):
             # Get visibilities within the current time range
             targs = np.where(np.logical_and(lha[good_vis_idx] >= time_intervals[ts],
                                             lha[good_vis_idx] < time_intervals[ts + 1]))
             if np.size(targs) > 0:
-                lha_mean.append(np.mean(lha[good_vis_idx[targs]]))
+                lha_planes.append(np.mean(lha[good_vis_idx[targs]]))
                 vis_timestep[good_vis_idx[targs]] = ts
             else:
-                lha_mean.append(0.0)
+                lha_planes.append(0.0)
 
         # If matrix rotation is enabled, we generate A-kernel once before the gridding procedure
-        if aproj_optimisation or aproj_interp_rotation is True:
+        if aproj_opt or aproj_interp_rotation is True:
             fov = image_size * cell_size.to(u.rad).value
-            pbeam = akernel.generate_primary_beam(pbeam_coefs, fov, workarea_size)
+            pbeam = akernel_generation.generate_akernel(pbeam_coefs, fov, workarea_size)
     else:
         # Set 1 time step when A-projection is False (just to enter in gridding loop. A-projection is not performed)
         num_timesteps = 1
@@ -298,7 +300,7 @@ def convolve_to_grid(kernel_func,
                                cell_size=cell_size.to(u.rad).value, undersampling=undersampling_ratio,
                                radial_line=radial_line)
             # If aproj-optimisation is set then generate the oversampled convolution kernel using unrotated a-kernel
-            if use_aproj and aproj_optimisation is True:
+            if use_aproj and aproj_opt is True:
                 oversampled_conv_kernel, conv_support = \
                     generate_oversampled_convolution_kernel(w_kernel, aa_kernel_img_array, workarea_size,
                                                             max_wpconv_support, oversampling, pbeam, hankel_opt,
@@ -307,19 +309,19 @@ def convolve_to_grid(kernel_func,
         for ts in range(num_timesteps):
             if use_aproj is True:
                 # Generate the AW-kernels
-                if aproj_optimisation is True:
+                if aproj_opt is True:
                     # Due to aproj-optimisation we just need to rotate the convolution kernel for each LHA
                     kernel_cache = populate_kernel_cache_awprojection(oversampled_conv_kernel, conv_support,
-                                                                      oversampling, True, lha_mean[ts],
+                                                                      oversampling, True, lha_planes[ts],
                                                                       np.deg2rad(obs_dec), np.deg2rad(obs_ra))
                 else:
                     # Non-optimised A-projection: rotate a-kernel before multiplying by w- and aa-kernels
                     if aproj_interp_rotation is True:
-                        a_kernel = akernel.rotate_primary_beam_for_lha(pbeam, lha_mean[ts], np.deg2rad(obs_dec),
+                        a_kernel = akernel_generation.rotate_akernel_by_lha(pbeam, lha_planes[ts], np.deg2rad(obs_dec),
                                                                        np.deg2rad(obs_ra))
                     else:
                         fov = image_size * cell_size.to(u.rad).value
-                        a_kernel = akernel.generate_primary_beam_for_lha(pbeam_coefs, lha_mean[ts], np.deg2rad(obs_dec),
+                        a_kernel = akernel_generation.generate_akernel_from_lha(pbeam_coefs, lha_planes[ts], np.deg2rad(obs_dec),
                                                                          np.deg2rad(obs_ra), fov, workarea_size)
 
                     # Multiply a-, w- and aa-kernels, and determine FFT
@@ -377,7 +379,7 @@ def convolve_to_grid(kernel_func,
                 if progress_bar is not None:
                     progress_bar.update(1)
 
-    return vis_grid, sampling_grid
+    return vis_grid, sampling_grid, lha_planes
 
 
 def _bounds_check_kernel_centre_locations(uv, kernel_centre_indices,
@@ -654,7 +656,7 @@ def populate_kernel_cache_awprojection(oversampled_conv_kernel, trunc_conv_sup, 
 
     if rotate is True:
         # Determine parallactic angle (in radians)
-        pangle = akernel.parallatic_angle(lha, np.deg2rad(dec_rad), np.deg2rad(ra_rad))
+        pangle = akernel_generation.parallatic_angle(lha, dec_rad, ra_rad)
 
         min_val = oversampled_conv_kernel[0, 0]
         rot_oversampled_conv_kernel = np.empty_like(oversampled_conv_kernel)
@@ -689,7 +691,7 @@ def compute_wplanes(good_vis_idx, num_wplanes, w_lambda, wplanes_median):
     Args:
         good_vis_idx (numpy.array): List of good visibility indexes.
         num_wplanes (int): Number of W-planes to compute.
-        w_lambda (numpy.array): List of W-lambda values for each visibility.
+        w_lambda (numpy.array): W-lambda values for each visibility.
         wplanes_median (bool): If true, determine W-plane values using the median rather than mean.
 
     Returns:

src/fastimgproto/imager.py
@@ -1,8 +1,10 @@
 import astropy.units as u
 import numpy as np
+from numpy.core.multiarray import dtype
 
 from fastimgproto.gridder.gridder import convolve_to_grid
 from fastimgproto.gridder.kernel_generation import ImgDomKernel
+from fastimgproto.gridder import akernel_generation
 
 
 def fft_to_image_plane(uv_grid):
@@ -33,8 +35,8 @@ def image_visibilities(
         obs_ra=0.0,
         lha=np.ones(1,),
         pbeam_coefs=np.array([1]),
-        aproj_interp_rotation=False,
-        aproj_optimisation=False,
+        aproj_opt=False,
+        aproj_mask_perc=0.0,
         progress_bar=None):
     """
     Args:
@@ -93,10 +95,11 @@ def image_visibilities(
             1d array, shape: `(n_vis,)`.
         pbeam_coefs (numpy.ndarray): Primary beam given by spherical harmonics coefficients.
             The SH degree is constant being derived from the number of coefficients minus one.
-        aproj_interp_rotation (bool): Use interpolation techniques for primary beam rotation
-            in A-projection instead of recomputing a-kernel from spherical harmonics.
-        aproj_optimisation (bool): Use A-projection optimisation which rotates the
+        aproj_opt (bool): Use A-projection optimisation which rotates the
             convolution kernel rather than the A-kernel.
+        aproj_mask_perc (float): Threshold value (in percentage) used to detect near zero
+            regions of the primary beam. The output dirty image and beam are masked in
+            the same regions to hide the boosted noise.
         progress_bar (tqdm.tqdm): [Optional] progressbar to update.
 
     Returns:
@@ -136,33 +139,32 @@ def image_visibilities(
     uvw_in_pixels = (uvw_lambda / grid_pixel_width_lambda).value
     uv_in_pixels = uvw_in_pixels[:, :2]
 
-    vis_grid, sample_grid = convolve_to_grid(kernel_func,
-                                             aa_support=kernel_support,
-                                             image_size=image_size_int,
-                                             uv=uv_in_pixels,
-                                             vis=vis,
-                                             vis_weights=vis_weights,
-                                             exact=kernel_exact,
-                                             oversampling=kernel_oversampling,
-                                             num_wplanes=num_wplanes,
-                                             cell_size=cell_size,
-                                             w_lambda=uvw_lambda[:, 2],
-                                             wplanes_median=wplanes_median,
-                                             max_wpconv_support=max_wpconv_support,
-                                             analytic_gcf=analytic_gcf,
-                                             hankel_opt=hankel_opt,
-                                             interp_type=interp_type,
-                                             undersampling_opt=undersampling_opt,
-                                             kernel_trunc_perc=kernel_trunc_perc,
-                                             aproj_numtimesteps=aproj_numtimesteps,
-                                             obs_dec=obs_dec,
-                                             obs_ra=obs_ra,
-                                             lha=lha,
-                                             pbeam_coefs=pbeam_coefs,
-                                             aproj_interp_rotation=aproj_interp_rotation,
-                                             aproj_optimisation=aproj_optimisation,
-                                             progress_bar=progress_bar
-                                             )
+    vis_grid, sample_grid, lha_planes = convolve_to_grid(kernel_func,
+                                                         aa_support=kernel_support,
+                                                         image_size=image_size_int,
+                                                         uv=uv_in_pixels,
+                                                         vis=vis,
+                                                         vis_weights=vis_weights,
+                                                         exact=kernel_exact,
+                                                         oversampling=kernel_oversampling,
+                                                         num_wplanes=num_wplanes,
+                                                         cell_size=cell_size,
+                                                         w_lambda=uvw_lambda[:, 2],
+                                                         wplanes_median=wplanes_median,
+                                                         max_wpconv_support=max_wpconv_support,
+                                                         analytic_gcf=analytic_gcf,
+                                                         hankel_opt=hankel_opt,
+                                                         interp_type=interp_type,
+                                                         undersampling_opt=undersampling_opt,
+                                                         kernel_trunc_perc=kernel_trunc_perc,
+                                                         aproj_numtimesteps=aproj_numtimesteps,
+                                                         obs_dec=obs_dec,
+                                                         obs_ra=obs_ra,
+                                                         lha=lha,
+                                                         pbeam_coefs=pbeam_coefs,
+                                                         aproj_opt=aproj_opt,
+                                                         progress_bar=progress_bar
+                                                         )
 
     # Perform FFT step
     image = np.real(fft_to_image_plane(vis_grid))
@@ -173,7 +175,7 @@ def image_visibilities(
     if gridding_correction is True:
         # Generate correction kernel
         gcf_array = ImgDomKernel(kernel_func, image_size_int, normalize=False, radial_line=False,
-                             analytic_gcf=analytic_gcf).array
+                                 analytic_gcf=analytic_gcf).array
         # Normalization factor:
         # We correct for the FFT scale factor of 1/image_size**2 by dividing by the image-domain AA-kernel
         # (cf https://docs.scipy.org/doc/numpy/reference/routines.fft.html#implementation-details)
@@ -187,5 +189,16 @@ def image_visibilities(
     if total_sample_weight != 0:
         image = image / gcf_array
         beam = beam / gcf_array
+
+    # Mask circular regions in the image where A-kernel is near zero
+    if aproj_numtimesteps > 0 and aproj_mask_perc > 0.0:
+        fov = image_size_int * cell_size.to(u.rad).value
+        akernel_mask = np.ones_like(image, dtype=bool)
+        for lha_value in lha_planes:
+            akernel = akernel_generation.generate_akernel_from_lha(pbeam_coefs, lha_value, np.deg2rad(obs_dec),
+                                                                   np.deg2rad(obs_ra), fov, image_size_int)
+            akernel_mask = np.logical_and(akernel_mask, np.less(akernel, 100.0 / aproj_mask_perc))
+        image = image * akernel_mask
+        beam = beam * akernel_mask
 
     return image, beam

src/fastimgproto/scripts/reduce.py
@@ -173,7 +173,8 @@ def main(uvw_lambda,
     obs_dec = aprojection_config[ConfigKeys.obs_dec]
     obs_ra = aprojection_config[ConfigKeys.obs_ra]
     pbeam_coefs = aprojection_config[ConfigKeys.pbeam_coefs]
-    aproj_interp_rotation = aprojection_config[ConfigKeys.aproj_interp_rotation]
+    aproj_opt = aprojection_config[ConfigKeys.aproj_opt]
+    aproj_mask_perc = aprojection_config[ConfigKeys.aproj_mask_perc]
 
     # Sourcefind settings
     detection_n_sigma = sourcefind_config[ConfigKeys.sourcefind_detection]
@@ -207,7 +208,8 @@ def main(uvw_lambda,
                                                 obs_ra=obs_ra,
                                                 lha=lha,
                                                 pbeam_coefs=pbeam_coefs,
-                                                aproj_interp_rotation=aproj_interp_rotation,
+                                                aproj_opt=aproj_opt,
+                                                aproj_mask_perc=aproj_mask_perc,
                                                 progress_bar=progress_bar)
     logger.info("Running sourcefinder on image")
     sfimage = SourceFindImage(data=np.real(image),